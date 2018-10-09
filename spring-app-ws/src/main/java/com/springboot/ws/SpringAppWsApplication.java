125b6e884ce8d75a79f2681bbe4232eaf6c5193a
xionger <xunhao.xiong@gmail.com>
2018-10-09 16:55:00 -0500

    first commit

---
+++ spring-app-ws/src/main/java/com/springboot/ws/SpringAppWsApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppWsApplication.class, args);
+	}
+}
