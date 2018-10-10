a9382e4a433a5be5af79137d07332601a64a4c86
xionger <xunhao.xiong@gmail.com>
2018-10-09 22:49:48 -0500

    implement user entity

---
+++ spring-app-ws/src/main/java/com/spring/app/ws/service/UserService.java
@@ -0,0 +1,7 @@
+package com.spring.app.ws.service;
+
+import com.spring.app.ws.shared.dto.UserDto;
+
+public interface UserService {
+	UserDto createUser(UserDto user);
+}
---
+++ spring-app-ws/src/main/java/com/spring/app/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.spring.app.ws.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.spring.app.ws.service.UserService;
+import com.spring.app.ws.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDto createUser(UserDto user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
