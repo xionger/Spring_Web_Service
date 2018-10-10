c82fbef0ef75a3a8484ba30e57b29c9fbc83055d
xionger <xunhao.xiong@gmail.com>
2018-10-09 21:26:35 -0500

    implement userrequestmodel and userrest

--- spring-app-ws/src/main/java/com/spring/app/ws/ui/controller/UserController.java
+++ spring-app-ws/src/main/java/com/spring/app/ws/ui/controller/UserController.java
@@ -1,10 +1,37 @@
 package com.spring.app.ws.ui.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.spring.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
-
+	
+	@GetMapping
+	public String getUser() {
+		return "getuser is called";
+	}
+	
+	@PostMapping
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		return null;
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "updateuser is called";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "deleteuser is called";
+	}
 }
