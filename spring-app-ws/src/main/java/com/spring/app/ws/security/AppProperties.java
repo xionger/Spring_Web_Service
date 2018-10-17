0d7de3615669a09b33bac4e3dd56e3a590f5c6cc
xionger <xunhao.xiong@gmail.com>
2018-10-16 22:54:00 -0500

    get token secret from property

---
+++ spring-app-ws/src/main/java/com/spring/app/ws/security/AppProperties.java
@@ -0,0 +1,15 @@
+package com.spring.app.ws.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppProperties {
+	@Autowired
+	private Environment env;
+	
+	public String getTokenSecret() {
+		return env.getProperty("tokenSecret");
+	}
+}
--- spring-app-ws/src/main/java/com/spring/app/ws/security/SecurityConstants.java
+++ spring-app-ws/src/main/java/com/spring/app/ws/security/SecurityConstants.java
@@ -1,9 +1,17 @@
 package com.spring.app.ws.security;
 
+import com.spring.app.ws.SpringApplicationContext;
+
 public class SecurityConstants {
     public static final long EXPIRATION_TIME = 864000000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
-    public static final String TOKEN_SECRET = "skfkj47fska";
+    //public static final String TOKEN_SECRET = "skfkj47fska";
+    
+    public static String getTokenSecret()
+    {
+        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+        return appProperties.getTokenSecret();
+    }
 }
